e72b53b0e7198365208521fad27fcf57a6145336
LMalko <lmalko2601@gmail.com>
2018-04-16 01:20:44 +0200

    Add various challenges

---
+++ 00 Various challenges/part 01.py
@@ -0,0 +1,22 @@
+# 01. Create a dictionary out of list1(keys) & list2(values).
+
+def two_list_dictionary(a,b):
+    return {**{}.fromkeys(a, "EMPTY"), **dict(zip(a,b))}
+
+print(two_list_dictionary(["a", "b", "c", "d", "e"], [1,2,3,4]))
+
+# 02. Create dictionary out of string letters occurence.
+
+from collections import Counter
+
+def strings_construction(string):
+    return dict(Counter(string))
+
+print(strings_construction("This is a test string, You can put anything here."))
+
+# 03. Check the length of the longest string.
+
+def check_for_longest(collection):
+    return len(max(collection, key=len))
+
+print(check_for_longest(["aaaaaaa", "bb", "cccccccc", "ddddddddddddd", "eeee"]))
