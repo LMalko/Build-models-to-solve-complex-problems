3720488718c88b559f7f1cded73e3119085d638a
LMalko <lmalko2601@gmail.com>
2018-04-18 20:34:12 +0200

    Add part02.py

--- 00 Various challenges/part 02.py
+++ 00 Various challenges/part 02.py
@@ -2,3 +2,8 @@
 
 print("aei23ou".translate(str.maketrans("aeiou", "uoiea")))
 
+# 02. Filter elements to list under condition.
+
+    # Filter only positive numbers NOT REPEATING:
+lista = [-3, -4, -1, 2, -7, 4]
+print(list(filter(lambda x: x > 0, lista)))
