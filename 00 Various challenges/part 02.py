849d5145d4062736ce81f05e2ef89167e355c33d
LMalko <lmalko2601@gmail.com>
2018-04-18 20:47:17 +0200

    Add part02.py

--- 00 Various challenges/part 02.py
+++ 00 Various challenges/part 02.py
@@ -7,3 +7,10 @@ print("aei23ou".translate(str.maketrans("aeiou", "uoiea")))
     # Filter only positive numbers NOT REPEATING:
 lista = [-3, -4, -1, 2, -7, 4]
 print(list(filter(lambda x: x > 0, lista)))
+
+#03. Sort under condition without changing elements.
+
+lista = ["a", "C", "b", "a", "c", "A"]
+print(sorted(lista, key=lambda x: x.lower(), reverse=True))
+    # min(lista, key=lambda number: number * number)
+    # max(lista, key=lambda number: number * number)
