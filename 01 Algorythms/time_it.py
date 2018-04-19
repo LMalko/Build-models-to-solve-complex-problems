91073e7b682cbdcffa665ab03a26c087080d6a35
LMalko <lmalko2601@gmail.com>
2018-04-19 20:55:05 +0200

    Add time_it_with_recursion in time_it.py

--- 01 Algorythms/time_it.py
+++ 01 Algorythms/time_it.py
@@ -6,6 +6,21 @@ def time_it(function):
         start = time.time()
         result = function(*args, *kwargs)
         end = time.time()
-        print(function.__name__ + " took " + str((end - start) * 1000) + " mil sec")
+        time_in_milsec = (end - start) * 1000
+        print("{} took {} mil sec.".format(function.__name__, str(time_in_milsec)))
+        return result
+    return wrapper
+
+
+def time_it_for_recursive(function):
+    time_it.total = 0
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = function(*args, *kwargs)
+        end = time.time()
+        time_in_milsec = (end - start) * 1000
+        print("Latest {} took {} mil sec.".format(function.__name__, str(time_in_milsec)))
+        time_it.total += time_in_milsec
+        print("Latest total time is {} mil sec".format(str(time_it.total)))
         return result
     return wrapper
