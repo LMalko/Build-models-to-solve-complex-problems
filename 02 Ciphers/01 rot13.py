9cd58b478a295da2e8805618538b43a2b592be12
LMalko <lmalko2601@gmail.com>
2018-04-19 20:08:10 +0200

    Refactor rot13

--- 02 Ciphers/01 rot13.py
+++ 02 Ciphers/01 rot13.py
@@ -2,10 +2,10 @@ from string import ascii_lowercase , ascii_uppercase
 
 def rot13(message):
     return message.translate(str.maketrans(ascii_lowercase + ascii_uppercase,
-                                           "".join(ascii_lowercase[13:] +
-                                           ascii_lowercase[:13] +
-                                           ascii_uppercase [13:] +
-                                           ascii_uppercase [:13])))
+                                                  "".join(ascii_lowercase[13:] +
+                                                  ascii_lowercase[:13] +
+                                                  ascii_uppercase [13:] +
+                                                  ascii_uppercase [:13])))
 
 
 string = "Pnrfne pvcure? V zhpu cersre Pnrfne fnynq!"
