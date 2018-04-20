4fbdd73255eea37044c76040235a26909639de0d
LMalko <lmalko2601@gmail.com>
2018-04-20 18:36:31 +0200

    Add affine caesar

---
+++ 02 Ciphers/03 affine.py
@@ -0,0 +1,39 @@
+from string import ascii_lowercase
+import re
+
+def affine(message, slope_a, intercept_b, decrypt=False):
+    letters_to_numbers = {**{}.fromkeys(ascii_lowercase, "EMPTY"),
+                          **dict(zip(ascii_lowercase,list(range(26))))}
+    numbers_to_letters = {**{}.fromkeys(list(range(26)), "EMPTY"),
+                          **dict(zip(list(range(26)), ascii_lowercase))}
+    allowed_factors_for_a = [1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25]
+    letters = re.compile("[a-zA-Z]")
+    def encipher(letter):
+        return numbers_to_letters[(slope_a * letters_to_numbers[letter] + intercept_b) % 26]
+    def decipher(letter):
+
+        # Find a^-1 (the multiplicative inverse / Euclidean algorythm)
+        for i in allowed_factors_for_a:
+            temp = (i * slope_a) % 26
+            if temp == 1:
+                multiplicative_inverse_of_a = i
+        return numbers_to_letters[(multiplicative_inverse_of_a
+                                       * (letters_to_numbers[letter] - intercept_b)) % 26]
+
+    if slope_a % 26 not in allowed_factors_for_a:
+        return "This key a is not allowed."
+    if decrypt:
+        return "".join(list(map(lambda x: decipher(x) if letters.match(x) else x, message.lower())))
+    return "".join(list(map(lambda x: encipher(x) if letters.match(x) else x, message.lower())))
+
+
+string = "This message should be displayed from the second print 68."
+
+test_a = 45
+test_b = 67677
+
+encrypted = affine(string,test_a, test_b)
+print(encrypted)
+print(affine(encrypted, test_a, test_b, True))
+
+
