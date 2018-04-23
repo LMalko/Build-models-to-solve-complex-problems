ae7eb1045d7a1a8abcf75bc97096f02523c39ff9
LMalko <lmalko2601@gmail.com>
2018-04-23 14:34:45 +0200

    Add assign names to regex groups in part 02.py

--- 00 Various challenges/part 02.py
+++ 00 Various challenges/part 02.py
@@ -109,3 +109,14 @@ print(result)
 alternatingDigitPairs= re.search(r'(\d)(\d)\1\2', postalCode).group()
 print(alternatingDigitPairs)
 
+# 10.10 Assign names to regex groups.
+
+def parse_name(input):
+	name_regex = re.compile(r'^(Mr\.|Mrs\.|Ms\.|Mdme\.) (?P<first>[A-Za-z]+) (?P<last>[A-Za-z]+)$')
+	matches = name_regex.search(input)
+	print(matches.group())
+	print(matches.group('first'))
+	print(matches.group('last'))
+
+parse_name("Mrs. Grazyna Wojcik")
+
