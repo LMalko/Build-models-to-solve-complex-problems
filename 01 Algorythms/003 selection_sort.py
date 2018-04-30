e5da911d38e590b6b267ae11694bfde1f2205c2f
LMalko <lmalko2601@gmail.com>
2018-04-30 19:03:56 +0200

    Comment out big sort in selection_sort

--- 01 Algorythms/003 selection_sort.py
+++ 01 Algorythms/003 selection_sort.py
@@ -1,17 +1,30 @@
-
+import copy
 import random
 import sys
 sys.path.append("..")
 from resources.time_it import time_it
 
-@time_it_for_recursive
+@time_it
 def selection_sort(collection):
-   pass
+    start = 0
 
+    for i in range(len(collection)):
+        minimum = collection[start]
+        swapped = False
+        while not swapped:
+            swapped = True
+            for index, value in enumerate(collection[start:]):
+                if value < minimum:
+                    swapped = False
+                    temp_index = index
+                    minimum = value
+            if not swapped:
+                collection[temp_index + start], collection[start] = collection[start], minimum
+        start += 1
+    return collection
 
-
-small_list = [1,]
-big_list = [1,]
+small_list = [40,]
+big_list = [40,]
 
 
 while(len(small_list) < 100):
@@ -23,4 +36,5 @@ while(len(big_list) < 50000):
     big_list.append(number)
 
 print(selection_sort(small_list))
-print(selection_sort(big_list))
+# print(selection_sort(big_list))
+
