407824f3d81ff2c0208d92f36edcbb5ac9ab284b
LMalko <lmalko2601@gmail.com>
2018-05-23 19:15:15 +0200

    Write node.py

--- 04 Data Structures/01 Linked List/node.py
+++ 04 Data Structures/01 Linked List/node.py
@@ -1,13 +1,13 @@
 class Node:
 
     def __init__(self, name, matric, year):
-        self.name = name
+        self.__name = name
         self.matric = matric
         self.year = year
         self.__next = None
 
     def set_next(self, node):
-        if isinstance(node, Node) or node is None:
+        if isinstance(node, Node) or node:
             self.__next = node
         else:
             raise TypeError("The 'next' node must be of type Node or None")
@@ -16,4 +16,7 @@ class Node:
         return self.__next
 
     def display_node(self):
-        print(f"{self.matric}: {self.name} (year {self.year})")
+        print(f"{self.matric}: {self.__name} (year {self.year})")
+
+    def get_name(self):
+        return self.__name
